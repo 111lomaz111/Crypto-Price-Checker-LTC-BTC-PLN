8dc34cf2f71b5dc26b8ba3ce426b3dd7ac86ca93
lomaz <lomaz111@poczta.onet.pl>
2017-12-15 23:20:53 +0100

    added repo

---
+++ LTCPriceChecker/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LTCPriceChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxuserLTCAmount = new System.Windows.Forms.TextBox();
+            this.labelPutYourLTCAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(174, 70);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxuserLTCAmount
+            // 
+            this.textBoxuserLTCAmount.Location = new System.Drawing.Point(126, 88);
+            this.textBoxuserLTCAmount.Name = "textBoxuserLTCAmount";
+            this.textBoxuserLTCAmount.Size = new System.Drawing.Size(60, 20);
+            this.textBoxuserLTCAmount.TabIndex = 1;
+            this.textBoxuserLTCAmount.Text = "0";
+            this.textBoxuserLTCAmount.TextChanged += new System.EventHandler(this.textBoxuserLTCAmount_TextChanged);
+            // 
+            // labelPutYourLTCAmount
+            // 
+            this.labelPutYourLTCAmount.AutoSize = true;
+            this.labelPutYourLTCAmount.Location = new System.Drawing.Point(12, 91);
+            this.labelPutYourLTCAmount.Name = "labelPutYourLTCAmount";
+            this.labelPutYourLTCAmount.Size = new System.Drawing.Size(35, 13);
+            this.labelPutYourLTCAmount.TabIndex = 2;
+            this.labelPutYourLTCAmount.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(196, 115);
+            this.Controls.Add(this.labelPutYourLTCAmount);
+            this.Controls.Add(this.textBoxuserLTCAmount);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LTC Value";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxuserLTCAmount;
+        private System.Windows.Forms.Label labelPutYourLTCAmount;
+    }
+}
+
---
+++ LTCPriceChecker/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace LTCPriceChecker
+{
+    public partial class Form1 : Form
+    {
+
+        private String html;
+        private float LTCValuePLN, userLTCAmount, userLTCValuePLN;
+        private WebClient webClient;
+        private Thread retakeValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+            labelPutYourLTCAmount.Text = "Put Your LTC Amount:";
+            userLTCAmount = 0;
+            LTCValuePLN = 0;
+            userLTCValuePLN =0;
+            TopMost = true; //allways on top of screen
+        }
+
+        private void textBoxuserLTCAmount_TextChanged(object sender, EventArgs e)
+        {
+            userLTCAmount = Single.Parse(textBoxuserLTCAmount.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainFunction();
+            convertValueToPLN();
+            //something
+        }
+
+        //creating and connecting web client, then is  adding values 
+        private void mainFunction()
+        {
+            webClient = new WebClient();
+
+            using (webClient)
+            {
+                //test
+                try
+                {
+                    html = webClient.DownloadString("https://bitmarket24.pl/api/LTC_PLN/transactions.json");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+            MatchCollection m1 = Regex.Matches(html, @"\d+((.|,)\d)+", RegexOptions.Singleline);
+
+            int i = 0;
+            foreach (Match m in m1)
+            {
+                float.TryParse(m.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out LTCValuePLN);
+                i++;
+                if (i == 1) break;
+            }
+        }
+
+        //recalculating user value from ltc value to value in pln
+        private void convertValueToPLN()
+        {
+            retakeValue = new Thread(
+                new ThreadStart(() =>
+                {
+                    for (; ; )
+                    {
+                        mainFunction();
+                        userLTCAmount = Single.Parse(textBoxuserLTCAmount.Text);
+                        userLTCValuePLN = LTCValuePLN * userLTCAmount;
+
+                        Thread.Sleep(1000);
+
+                        Invoke(new Action(() =>
+                        {
+                            richTextBox1.Text = "LTC Price in PLN = " + LTCValuePLN + " PLN";
+                            richTextBox1.Text += "\n" + "Your LTC value in PLN = " + userLTCValuePLN.ToString();
+                        }));
+                    }
+                }
+                ));
+            retakeValue.Start();
+        }
+    }
+}
