fa664bc586e2e5968f37c7a7ff9a8a938fa19555
lomaz <lomaz111@poczta.onet.pl>
2017-12-18 11:34:50 +0100

    added btc

--- LTCPriceChecker/Form1.Designer.cs
+++ LTCPriceChecker/Form1.Designer.cs
@@ -31,17 +31,20 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBoxuserLTCAmount = new System.Windows.Forms.TextBox();
             this.labelPutYourLTCAmount = new System.Windows.Forms.Label();
+            this.labelPutYourBTCAmount = new System.Windows.Forms.Label();
+            this.textBoxuserBTCAmount = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
             this.richTextBox1.Size = new System.Drawing.Size(174, 70);
             this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.richTextBox1.Text = "RESPONE";
             // 
             // textBoxuserLTCAmount
             // 
@@ -49,24 +52,42 @@
             this.textBoxuserLTCAmount.Name = "textBoxuserLTCAmount";
             this.textBoxuserLTCAmount.Size = new System.Drawing.Size(60, 20);
             this.textBoxuserLTCAmount.TabIndex = 1;
-            this.textBoxuserLTCAmount.Text = "0";
-            this.textBoxuserLTCAmount.TextChanged += new System.EventHandler(this.textBoxuserLTCAmount_TextChanged);
+            this.textBoxuserLTCAmount.Text = "0,39015";
             // 
             // labelPutYourLTCAmount
             // 
             this.labelPutYourLTCAmount.AutoSize = true;
             this.labelPutYourLTCAmount.Location = new System.Drawing.Point(12, 91);
             this.labelPutYourLTCAmount.Name = "labelPutYourLTCAmount";
-            this.labelPutYourLTCAmount.Size = new System.Drawing.Size(35, 13);
+            this.labelPutYourLTCAmount.Size = new System.Drawing.Size(123, 13);
             this.labelPutYourLTCAmount.TabIndex = 2;
-            this.labelPutYourLTCAmount.Text = "label1";
+            this.labelPutYourLTCAmount.Text = "labelPutYourLTCAmount";
+            // 
+            // labelPutYourBTCAmount
+            // 
+            this.labelPutYourBTCAmount.AutoSize = true;
+            this.labelPutYourBTCAmount.Location = new System.Drawing.Point(12, 117);
+            this.labelPutYourBTCAmount.Name = "labelPutYourBTCAmount";
+            this.labelPutYourBTCAmount.Size = new System.Drawing.Size(124, 13);
+            this.labelPutYourBTCAmount.TabIndex = 3;
+            this.labelPutYourBTCAmount.Text = "labelPutYourBTCAmount";
+            // 
+            // textBoxuserBTCAmount
+            // 
+            this.textBoxuserBTCAmount.Location = new System.Drawing.Point(126, 114);
+            this.textBoxuserBTCAmount.Name = "textBoxuserBTCAmount";
+            this.textBoxuserBTCAmount.Size = new System.Drawing.Size(60, 20);
+            this.textBoxuserBTCAmount.TabIndex = 4;
+            this.textBoxuserBTCAmount.Text = "0,39015";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(196, 115);
+            this.ClientSize = new System.Drawing.Size(196, 150);
+            this.Controls.Add(this.textBoxuserBTCAmount);
+            this.Controls.Add(this.labelPutYourBTCAmount);
             this.Controls.Add(this.labelPutYourLTCAmount);
             this.Controls.Add(this.textBoxuserLTCAmount);
             this.Controls.Add(this.richTextBox1);
@@ -86,6 +107,8 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBoxuserLTCAmount;
         private System.Windows.Forms.Label labelPutYourLTCAmount;
+        private System.Windows.Forms.Label labelPutYourBTCAmount;
+        private System.Windows.Forms.TextBox textBoxuserBTCAmount;
     }
 }
 
--- LTCPriceChecker/Form1.cs
+++ LTCPriceChecker/Form1.cs
@@ -17,44 +17,47 @@ namespace LTCPriceChecker
     public partial class Form1 : Form
     {
 
-        private String html;
-        private float LTCValuePLN, userLTCAmount, userLTCValuePLN;
-        private WebClient webClient;
+        private String ltcWEB, btcWEB;
+        private float LTCValuePLN, userLTCAmount, userLTCValuePLN, BTCValuePLN, userBTCAmount, userBTCValuePLN;
+        private WebClient LTCwebClient, BTCwebClient;
         private Thread retakeValue;
 
         public Form1()
         {
             InitializeComponent();
-            labelPutYourLTCAmount.Text = "Put Your LTC Amount:";
+
+            labelPutYourLTCAmount.Text = "Put your LTC amount:";
+            labelPutYourBTCAmount.Text = "Put your BTC amount:";
+
             userLTCAmount = 0;
             LTCValuePLN = 0;
             userLTCValuePLN =0;
-            TopMost = true; //allways on top of screen
-        }
 
-        private void textBoxuserLTCAmount_TextChanged(object sender, EventArgs e)
-        {
-            userLTCAmount = Single.Parse(textBoxuserLTCAmount.Text);
+            userBTCAmount = 0;
+            BTCValuePLN = 0;
+            userBTCValuePLN = 0;
+
+            TopMost = true; //allways on top of screen
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            mainFunction();
-            convertValueToPLN();
+            getValuesOfCrypto();
+            convertUserCryptoToPLN();
             //something
         }
 
-        //creating and connecting web client, then is  adding values 
-        private void mainFunction()
+        //creating and connecting web client, then adding values 
+        private void getValuesOfCrypto()
         {
-            webClient = new WebClient();
+            LTCwebClient = new WebClient();
+            BTCwebClient = new WebClient();
 
-            using (webClient)
+            using (LTCwebClient)
             {
-                //test
                 try
                 {
-                    html = webClient.DownloadString("https://bitmarket24.pl/api/LTC_PLN/transactions.json");
+                    ltcWEB = LTCwebClient.DownloadString("https://bitmarket24.pl/api/LTC_PLN/transactions.json");
                 }
                 catch (Exception e)
                 {
@@ -62,35 +65,71 @@ namespace LTCPriceChecker
                 }
             }
 
-            MatchCollection m1 = Regex.Matches(html, @"\d+((.|,)\d)+", RegexOptions.Singleline);
+            using (BTCwebClient)
+            {                
+                try
+                {
+                    btcWEB = BTCwebClient.DownloadString("https://bitmarket24.pl/api/BTC_PLN/transactions.json");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
 
-            int i = 0;
-            foreach (Match m in m1)
+
+
+            MatchCollection ltc1 = Regex.Matches(ltcWEB, @"\d+((.|,)\d)+", RegexOptions.Singleline);
+
+            int l = 0;
+            foreach (Match ltc in ltc1)
             {
-                float.TryParse(m.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out LTCValuePLN);
-                i++;
-                if (i == 1) break;
+                float.TryParse(ltc.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out LTCValuePLN);
+                l++;
+                if (l == 1) break; //i = 1 is the newest price same in BTC \/
+            }
+
+            MatchCollection btc1 = Regex.Matches(btcWEB, @"\d+((.|,)\d)+", RegexOptions.Singleline);
+            int b = 0;
+            foreach (Match btc in btc1)
+            {
+                float.TryParse(btc.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out BTCValuePLN);
+                b++;
+                if (b == 1) break;
             }
         }
 
         //recalculating user value from ltc value to value in pln
-        private void convertValueToPLN()
+        private void convertUserCryptoToPLN()
         {
             retakeValue = new Thread(
                 new ThreadStart(() =>
                 {
                     for (; ; )
                     {
-                        mainFunction();
-                        userLTCAmount = Single.Parse(textBoxuserLTCAmount.Text);
-                        userLTCValuePLN = LTCValuePLN * userLTCAmount;
-
                         Thread.Sleep(1000);
+
+                        getValuesOfCrypto();
+
+                        try
+                        {
+                            userLTCAmount = Single.Parse(textBoxuserLTCAmount.Text);
+                            userBTCAmount = Single.Parse(textBoxuserBTCAmount.Text);
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show("Type your value in correct format: \"12,34\"");
+                        }
+
+                        userLTCValuePLN = LTCValuePLN * userLTCAmount;
+                        userBTCValuePLN = BTCValuePLN * userBTCAmount;
 
                         Invoke(new Action(() =>
                         {
-                            richTextBox1.Text = "LTC Price in PLN = " + LTCValuePLN + " PLN";
-                            richTextBox1.Text += "\n" + "Your LTC value in PLN = " + userLTCValuePLN.ToString();
+                            richTextBox1.Text = "LTC Price in PLN = " + LTCValuePLN;
+                            richTextBox1.Text += "\nBTC Price in PLN = " + BTCValuePLN;
+                            richTextBox1.Text += "\nYour LTC value in PLN = " + userLTCValuePLN.ToString();
+                            richTextBox1.Text += "\nYour BTC value in PLN = " + userBTCValuePLN.ToString();
                         }));
                     }
                 }
